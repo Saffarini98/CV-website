080dff70e7242986991419709bf70e54a746c83f
Maxim Saffarini <maximsaffarini@gmail.com>
2020-05-18 10:11:44 +0200

    index.html and css changes

--- public/js/main.js
+++ public/js/main.js
@@ -10,14 +10,10 @@
 //   }
 // });
 
-var page = document.querySelector(".page");
-var checkbox = document.querySelector('input[type="checkbox"]');
-checkbox.addEventListener("change", function() {
-  if (checkbox.checked) {
-    page.classList.remove("light-theme");
-    console.log("Not checked");
-  } else {
-    page.classList.add("light-theme");
-    console.log("Checked");
-  }
+const page = document.querySelector(".page");
+const checkbox = document.querySelector('input[type="checkbox"]');
+checkbox.addEventListener("change", () => {
+  checkbox.checked
+    ? page.classList.remove("light-theme")
+    : page.classList.add("light-theme");
 });
