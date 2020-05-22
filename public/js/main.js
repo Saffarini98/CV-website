7fe52c996377999412ff61b6b5702e86abcdb961
Maxim Saffarini <maximsaffarini@gmail.com>
2020-05-22 11:06:18 +0200

    Additional changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,61 @@
-// var page = document.querySelector(".page");
-// var checkbox = document.querySelector('input[type="checkbox"]');
-// checkbox.addEventListener("change", function() {
-//   if (checkbox.checked) {
-//     page.classList.toggle("dark-theme");
-//     console.log("Checked");
-//   } else {
-//     page.classList.toggle("light-theme");
-//     console.log("Not checked");
-//   }
-// });
-
+//==================// LigthTheme //==================//
 const page = document.querySelector(".page");
 const checkbox = document.querySelector('input[type="checkbox"]');
 checkbox.addEventListener("change", () => {
   checkbox.checked
     ? page.classList.remove("light-theme")
     : page.classList.add("light-theme");
+});
+
+//==================// Typing Effect //==================//
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = [
+  "HTML5, CSS3, Figma",
+  "Java, MongoDB, JavaScript",
+  "IntellJ IDEA, VS Code, Atom, Postman ",
+  "NodeJS, Express, APIs",
+  "LIFE in general I guess"
+];
+const typingDelay = 100;
+const erasingDelay = 100;
+const newTextDelay = 2000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if (!cursorSpan.classList.contains("typing"))
+      cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } else {
+    cursorSpan.classList.remove("typing");
+    setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    if (!cursorSpan.classList.contains("typing"))
+      cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(
+      0,
+      charIndex - 1
+    );
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  // On DOM Load initiate the effect
+  if (textArray.length) setTimeout(type, newTextDelay + 250);
 });
